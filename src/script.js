78ee5ba7130cd33e54c75e10806cc01f02593cb8
Aylascode <69477764+Aylascode@users.noreply.github.com>
2020-10-16 14:31:25 +0100

    Added last updated date/ time

--- src/script.js
+++ src/script.js
@@ -1,17 +1,33 @@
+function dateAndTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(responce) {
-  console.log(responce.data);
   let temperatureToday = document.querySelector("#temperature");
   let citySelected = document.querySelector("#city");
   let weatherDescribed = document.querySelector("#weatherDescription");
   let humidityToday = document.querySelector("#dayOneHumidity");
   let windSpeed = document.querySelector("#dayOneWindSpeed");
   let dayOneTemp = document.querySelector("#dayOneTempreture");
+  let UpdatedDayAndTime = document.querySelector("#todayAndTime");
   temperatureToday.innerHTML = Math.round(responce.data.main.temp);
   citySelected.innerHTML = responce.data.name;
   weatherDescribed.innerHTML = responce.data.weather[0].description;
   humidityToday.innerHTML = responce.data.main.humidity;
   windSpeed.innerHTML = Math.round(responce.data.wind.speed);
   dayOneTemp.innerHTML = Math.round(responce.data.main.temp);
+  UpdatedDayAndTime.innerHTML = dateAndTime(responce.data.dt * 1000);
 }
 
 let apiKey = "c3fb1aea0fcac49229fe92764b89263e";
@@ -19,5 +35,4 @@ let units = "metric";
 let city = "paris";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
-// console.log(apiUrl);
 axios.get(apiUrl).then(showTemperature);
